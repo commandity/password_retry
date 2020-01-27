6e898b02095779fc797bb39a332c2ceae07a8eb8
Jerzy Lin <commandity@gmail.com>
2020-01-27 14:35:08 +0800

    update

--- password.py
+++ password.py
@@ -2,13 +2,16 @@ password = 'a123456'
 turn = 3
 
 while turn > 0:
+	turn = turn - 1
 	pwd = input('請輸入密碼: ')
 	if pwd == password:
 		print('登入成功')
 		break
 	else:
-		turn = turn - 1
-		print('密碼錯誤! 還有', turn, '次機會')
-		if turn == 0:
+		print('密碼錯誤!')
+		if turn > 0:
+			print('還有', turn, '次機會')
+		else:
+			print('要被鎖帳號了')
 			break
 		
